1401b209b1099b351e5e625a073964f72ee74a25
Fatima20027 <fatima.hailou@icloud.com>
2025-11-13 15:13:56 +0100

    Initial commit

---
+++ js/egg.js
@@ -0,0 +1,51 @@
+//soft-boiled = 4min
+//jammy = 6min
+//hard-boiled = 8min
+//extra hard  = 10min
+
+
+const times = {
+  soft: 240,
+  jammy: 360,
+  hard: 480,
+  extra: 600
+};
+
+const counter = document.getElementById('counter');
+const alert = document.getElementById('alert')
+let timerinterval;
+
+
+function startTimer(value){
+  let selectedTime = times[value];
+
+  timerinterval = setInterval(() => {
+    const minutes = Math.floor(selectedTime / 60);
+    const seconds = Math.floor(selectedTime % 60);
+
+    const displayTime = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    counter.textContent = displayTime;
+
+    selectedTime--;
+
+    if(selectedTime < 0){
+      clearInterval(timerinterval)
+      alert.textContent = 'Your egg is done!'
+
+
+    }
+
+  }, 1000);
+
+  
+  
+}
+
+
+const urlPara = new URLSearchParams(window.location.search);
+const eggType = urlPara.get('egg');
+startTimer(eggType);
+
+
+
+
